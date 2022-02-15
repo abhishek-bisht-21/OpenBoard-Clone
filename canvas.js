6295cce3f3fdc338930ebafd67af1ef6203bdc75
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-15 07:59:45 +0530

    Adding Feature to draw on mouseDown

--- canvas.js
+++ canvas.js
@@ -2,9 +2,46 @@ let canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let mousedown = false;
+
 // API
 let tool = canvas.getContext("2d");
-tool.beginPath(); // new graphic (Path)
-tool.moveTo(10,10) // starting point
-tool.lineTo(100,150) // end point
-tool.stroke(); // Fill Graphic
+
+tool.strokeStyle = "red"
+tool.lineWidth = "3";
+
+
+// mousedown -> start new path, mousemove -> path fill (graphics)
+canvas.addEventListener("mousedown", (e) => {
+	mousedown = true;
+	beginPath({
+		x:e.clientX,
+		y:e.clientY
+	})
+	
+})
+
+canvas.addEventListener("mousemove", (e) => {
+
+	if(mousedown){
+		drawStroke({
+			x:e.clientX,
+			y:e.clientY
+		})
+	}
+	
+})
+
+canvas.addEventListener("mouseup",(e) => {
+	mousedown = false;
+})
+
+function beginPath(strokeObj){
+	tool.beginPath();
+	tool.moveTo(strokeObj.x,strokeObj.y);
+}
+
+function drawStroke(strokeObj){
+	tool.lineTo(strokeObj.x,strokeObj.y);
+	tool.stroke();
+}
