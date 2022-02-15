831f7edae3db0389293b123a1bed6ff0893768fc
Abhishek <abhishek.bisht54@yahoo.com>
2022-02-15 08:18:57 +0530

    Draw and Erase And Managing Width Size

--- canvas.js
+++ canvas.js
@@ -2,13 +2,22 @@ let canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let pencilColor = document.querySelectorAll(".pencil-color");
+let pencilWidthElem = document.querySelector(".pencil-width");
+let eraserWidthElem = document.querySelector(".eraser-width");
+
+let penColor = "red";
+let eraserColor = "white";
+let penWidth = pencilWidthElem.value;
+let eraserWidth = eraserWidthElem.value;
+
 let mousedown = false;
 
 // API
 let tool = canvas.getContext("2d");
 
-tool.strokeStyle = "red"
-tool.lineWidth = "3";
+tool.strokeStyle = "penColor"
+tool.lineWidth = "penWidth";
 
 
 // mousedown -> start new path, mousemove -> path fill (graphics)
@@ -45,3 +54,31 @@ function drawStroke(strokeObj){
 	tool.lineTo(strokeObj.x,strokeObj.y);
 	tool.stroke();
 }
+
+pencilColor.forEach((colorElem) => {
+	colorElem.addEventListener("click", (e) => {
+		let color = colorElem.classList[0];
+		penColor = color;
+		tool.strokeStyle = penColor;
+	})
+})
+
+pencilWidthElem.addEventListener("change", (e) => {
+	penWidth = pencilWidthElem.value;
+	tool.lineWidth = penWidth;
+})
+
+eraserWidthElem.addEventListener("change", (e) => {
+	eraserWidth = eraserWidthElem.value;
+	tool.lineWidth = eraserWidth;
+})
+
+eraser.addEventListener("click", (e) => {
+	if(eraserFlag){
+		tool.strokeStyle = eraserColor;
+		tool.lineWidth = eraserWidth;
+	}else{
+		tool.strokeStyle = penColor;
+		tool.lineWidth = penWidth;
+	}	
+})
